ed4ba9ed9dc1644e9edb0ff47cda05e5aa75e8de
shenyangyang <shenyangyang_91@163.com>
2017-08-11 18:54:08 +0800

    Init

--- js/common.js
+++ js/common.js
@@ -8,6 +8,32 @@
     if (r != null) return unescape(r[2]);
     return null;
   }
+  $.utils.dateParser = function(ct, format) {
+    if (!/^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$/.test(ct)) {
+        return '';
+    }
+
+    var tmpArr = ct.split(' '),
+        tmpDay = tmpArr[0].split('-'),
+        tmpTime = tmpArr[1].split(':');
+    var tmpDate = new Date(tmpDay[0], (parseInt(tmpDay[1]) - 1), tmpDay[2], tmpTime[0], tmpTime[1], tmpTime[2]);
+    var newDate = new Date();
+
+    var __methods = ['getFullYear', 'getMonth', 'getDate', 'getHours', 'getMinutes', 'getSeconds'], __flag = 0, __val = 0;
+    for (var i = 0; i < 6; i ++) {
+        var tmp = newDate[__methods[i]]() - tmpDate[__methods[i]]();
+        if (tmp > 0) {
+            __flag = i;
+            __val = tmp;
+            break;
+        }
+    }
+
+    if (format && Array.isArray(format) && format.length === 6) {
+        return format[__flag].replace('@', __val);
+    }
+    return '';
+}
   $.utils.validate = {
     mobile: function(v) {
       if ($.trim(v) && (/^[0-9]{11}$/.test(v))) {
